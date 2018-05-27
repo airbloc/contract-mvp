7568787da7c13c2e7aabe1e329e847eb65fdfbf0
Vista <retail3210@gmail.com>
2018-05-28 00:44:40 +0900

    Update project structures

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,39 @@
+require('babel-register')
+require('babel-polyfill')
+
+const newProvider = require('./config.local.provider.js')
+
+console.clear()
+
 module.exports = {
-  networks: {
-    development: {
-        host: "127.0.0.1",
-        port: 8545,
-        network_id: "*"
+    networks: {
+        test: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        mainnet: {
+            provider: newProvider('mainnet'),
+            network_id: 1,
+        },
+        ropsten: {
+            provider: newProvider('ropsten'),
+            network_id: 3,
+        },
+        rinkeby: {
+            provider: newProvider('rinkeby'),
+            network_id: 4,
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
     }
-  }
 };
